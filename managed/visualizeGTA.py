9e991874dcb5b676438d34fe2447bb92710644ed
Selcuk <aklanog@fzi.de>
2018-01-10 22:11:56 +0100

    2D transformation in python

--- managed/visualizeGTA.py
+++ managed/visualizeGTA.py
@@ -118,31 +118,44 @@ def convertToCamCord(pList, Camrot, Campos, camNearClip, camFOV, imgw, imgh, uiw
         #print("Xscale: %s"%str(Xscale))
         #print("Yscale: %s"%str(Yscale))
 
-        minX, minY, maxX, maxY = BB2D(Xscale, Yscale, uiw, uih)
-        
-        pListBB.append([(int(minX), int(minY)), (int(maxX), int(maxY))])
         pListProj.append((int(Xscale), int(Yscale)))
-        
-    return pListProj, pListBB
-
-def BB2D(x, y, uiw, uih):
+    
     minX = sys.maxint
     minY = sys.maxint
     maxX = 0
     maxY = 0
-
-    if x < minX: minX = x
-    if x > maxX: maxX = x
-    if y < minY: minY = y
-    if y > maxY: maxY = y
-
+    
+    i = 0
+    pOut = list()
+    for p in pListProj:
+        if p[0] < 0 or p[1] < 0:
+            i += 1
+            pOut.append(p)
+            continue
+        
+        if p[0] < minX: minX = p[0]
+        if p[0] > maxX: maxX = p[0]
+        if p[1] < minY: minY = p[1]
+        if p[1] > maxY: maxY = p[1]
+    
     if minX < 0: minX = 0
     if minY < 0: minY = 0
 
+    for o in pOut:
+        if o[0] < 0:
+            minX = 0
+        elif o[0] > uiw:
+            maxX = uiw
+        elif o[1] < 0:
+            minY = 0
+        elif o[1] > uih:
+            maxY = uih
+
     if maxX > uiw: maxX = uiw
     if maxY > uih: maxY = uih
-
-    return minX, minY, maxX, maxY
+    
+    pListBB.append([(int(minX), int(minY)), (int(maxX), int(maxY))])
+    return pListProj, pListBB
 
 def bbox_from_string(name):
     bbox2 = []
@@ -165,82 +178,6 @@ def bbox_from_string(name):
                     bbox3.append(BB3D)        
                     bbox2.append(BB2D) 
     return bbox3, bbox2
-
-def show_3dbounding_boxes(img, name, size, ax):
-    #FURGame - 0
-    #FULGame - 1
-    #BULGame - 2
-    #BURGame - 3
-    #FLRGame - 4
-    #FLLGame - 5
-    #BLLGame - 6
-    #BLRGame - 7
-    
-    corners, bb = bbox_from_string(name)
-    
-    codes1 = [Path.MOVETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.CLOSEPOLY,
-        ]
-        
-    codes2 = [Path.MOVETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.CLOSEPOLY,
-        ]
-         
-    codes3 = [Path.MOVETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.CLOSEPOLY,
-        ]
-        
-    codes4 = [Path.MOVETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.CLOSEPOLY,
-        ]
-        
-    #for c in corners:
-    #    x, y = zip(*c)
-    #    ax.plot(x,y, 'ro')
-        
-    #    path1 = Path([c[0], c[1], c[2], c[3], c[0]], codes1)
-    #    path2 = Path([c[4], c[5], c[6], c[7], c[4]], codes2)
-    #    path3 = Path([c[0], c[4], c[7], c[3], c[0]], codes3)
-    #    path4 = Path([c[1], c[5], c[6], c[2], c[1]], codes4)
-    #    patch1 = patches.PathPatch(path1, facecolor="none", lw=1, edgecolor='green')
-    #    patch2 = patches.PathPatch(path2, facecolor="none", lw=1, edgecolor='green')
-    #    patch3 = patches.PathPatch(path3, facecolor="none", lw=1, edgecolor='green')
-    #    patch4 = patches.PathPatch(path4, facecolor="none", lw=1, edgecolor='green')
-    #    ax.add_patch(patch1)
-    #    ax.add_patch(patch2)
-    #    ax.add_patch(patch3)
-    #    ax.add_patch(patch4)
-
-    img = np.array(img)
-
-    for bb in corners:		
-        img = cv2.line(img, (bb[0][0], bb[0][1]), (bb[1][0], bb[1][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[1][0], bb[1][1]), (bb[2][0], bb[2][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[2][0], bb[2][1]), (bb[3][0], bb[3][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[3][0], bb[3][1]), (bb[4][0], bb[4][1]), (255, 0, 0), 1)
-
-        img = cv2.line(img, (bb[4][0], bb[4][1]), (bb[5][0], bb[5][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[5][0], bb[5][1]), (bb[6][0], bb[6][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[6][0], bb[6][1]), (bb[7][0], bb[7][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[7][0], bb[7][1]), (bb[4][0], bb[4][1]), (255, 0, 0), 1)
-
-        img = cv2.line(img, (bb[1][0], bb[1][1]), (bb[5][0], bb[5][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[5][0], bb[5][1]), (bb[6][0], bb[6][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[6][0], bb[6][1]), (bb[2][0], bb[2][1]), (255, 0, 0), 1)
-        img = cv2.line(img, (bb[2][0], bb[2][1]), (bb[1][0], bb[1][1]), (255, 0, 0), 1)
-    return img
 
 def show_bounding_boxes(name, size, ax):
     #FURGame - 0
@@ -316,89 +253,6 @@ def show_bounding_boxes(name, size, ax):
         ax.add_patch(patch4)
     return
 
-def show_pbounding_boxes(im, name, size, ax):
-    bbox2 =[]
-    for d in data:
-        if d['Image'] == name:
-            pmat = np.array([d['PMatrix']["Values"]] , order='C').reshape(4,4)
-            vmat = np.array([d['VMatrix']["Values"]] , order='C').reshape(4,4)
-            print(pmat)
-            for i,p in enumerate(d['Detections']):
-                if p["Type"] == "car" and p["Visibility"]:
-
-                    vec1 = np.dot(np.dot(vmat, pmat), np.array([p["FURGame"]["X"],p["FURGame"]["Y"],p["FURGame"]["Z"],p["FURGame"]["Z"]]))
-                    res1 = np.array([vec1[0]/vec1[2], vec1[1]/vec1[2]])
-                    
-                    vec2 = np.dot(np.dot(vmat, pmat), np.array([p["FULGame"]["X"],p["FULGame"]["Y"],p["FULGame"]["Z"],p["FULGame"]["Z"]]))
-                    res2 = np.array([vec2[0]/vec2[2], vec2[1]/vec2[2]])
-
-                    vec3 = np.dot(np.dot(vmat, pmat), np.array([p["BULGame"]["X"],p["BULGame"]["Y"],p["BULGame"]["Z"],p["BULGame"]["Z"]]))
-                    res3 = np.array([vec3[0]/vec3[2], vec3[1]/vec3[2]])
-
-                    vec4 = np.dot(np.dot(vmat, pmat), np.array([p["BURGame"]["X"],p["BURGame"]["Y"],p["BURGame"]["Z"],p["BURGame"]["Z"]]))
-                    res4 = np.array([vec4[0]/vec4[2], vec4[1]/vec4[2]])
-
-                    vec5 = np.dot(np.dot(vmat, pmat), np.array([p["FLRGame"]["X"],p["FLRGame"]["Y"],p["FLRGame"]["Z"],p["FLRGame"]["Z"]]))
-                    res5 = np.array([vec5[0]/vec5[2], vec5[1]/vec5[2]])
-
-                    vec6 = np.dot(np.dot(vmat, pmat), np.array([p["FLLGame"]["X"],p["FLLGame"]["Y"],p["FLLGame"]["Z"],p["FLLGame"]["Z"]]))
-                    res6 = np.array([vec6[0]/vec6[2], vec6[1]/vec6[2]])
-
-                    vec7 = np.dot(np.dot(vmat, pmat), np.array([p["BLLGame"]["X"],p["BLLGame"]["Y"],p["BLLGame"]["Z"],p["BLLGame"]["Z"]]))
-                    res7 = np.array([vec7[0]/vec7[2], vec7[1]/vec7[2]])
-
-                    vec8 = np.dot(np.dot(vmat, pmat), np.array([p["BLRGame"]["X"],p["BLRGame"]["Y"],p["BLRGame"]["Z"],p["BLRGame"]["Z"]]))
-                    res8 = np.array([vec8[0]/vec8[2], vec8[1]/vec8[2]])
-
-                    bbox2.append([res1, res2, res3, res4, res5, res6, res7, res8])
-    
-    codes1 = [Path.MOVETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.CLOSEPOLY,
-        ]
-        
-    codes2 = [Path.MOVETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.CLOSEPOLY,
-        ]
-         
-    codes3 = [Path.MOVETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.CLOSEPOLY,
-        ]
-        
-    codes4 = [Path.MOVETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.LINETO,
-         Path.CLOSEPOLY,
-        ]
-        
-    for c in bbox2:
-        x, y = zip(*c)
-        ax.plot(x,y, 'ro')
-        
-        path1 = Path([c[0], c[1], c[2], c[3], c[0]], codes1)
-        path2 = Path([c[4], c[5], c[6], c[7], c[4]], codes2)
-        path3 = Path([c[0], c[4], c[7], c[3], c[0]], codes3)
-        path4 = Path([c[1], c[5], c[6], c[2], c[1]], codes4)
-        patch1 = patches.PathPatch(path1, facecolor="none", lw=1, edgecolor='black')
-        patch2 = patches.PathPatch(path2, facecolor="none", lw=1, edgecolor='black')
-        patch3 = patches.PathPatch(path3, facecolor="none", lw=1, edgecolor='black')
-        patch4 = patches.PathPatch(path4, facecolor="none", lw=1, edgecolor='black')
-        ax.add_patch(patch1)
-        ax.add_patch(patch2)
-        ax.add_patch(patch3)
-        ax.add_patch(patch4)
-    return
-
-
 def load_depth(name):
     if name not in depths:
         tiff_depth = tifffile.imread(os.path.join(in_directory, name.split(".")[0] + '-depth.tiff'))
@@ -463,14 +317,8 @@ def main():
         ax4.set_title('depth')
         ax4.imshow(load_depth(name), cmap='gray')
         
-        # Funzt momentan ned
-        #show_3dbounding_boxes(im, name, size, ax1)
-        
-        # Der lauft nicht!!
-        #show_pbounding_boxes(im, name, size, ax1)
-        
-        # Der funzt einigermassen gut
-        show_bounding_boxes(name, size, ax1)
+        # Der funzt einigermassen gut; Allg. Punktezeichnen bei Matplotlib ist nicht sonderlich gut
+        #show_bounding_boxes(name, size, ax1)
 
         plt.axis('off')
         plt.draw()
@@ -491,28 +339,91 @@ def main2():
                 imname = 'D:\\Devel\\GTAVisionExport\\managed\\Data\\' + splitname[0] + "_" + str(int(splitname[-1][:-5])+1) + ".tiff"
                 print(imname)
                 imgPil = Image.open(imname)
-                img = cv2.cvtColor(np.array(imgPil), cv2.COLOR_BGR2RGB)
+
+                # cv2.cvtColor macht am FZI Rechner Probleme!
+                #img = cv2.cvtColor(np.array(imgPil), cv2.COLOR_BGR2RGB)
+                img = np.array(imgPil)
+
                 if img == None: print("Image %s not found!"%('D:\\Devel\\GTAVisionExport\\managed\\Data\\' + name)); break
+
                 for i,p in enumerate(d['Detections']):
                     if p["Type"] == "car" and p["Visibility"]:
                         
-                        print((p["FUR"]["X"],p["FUR"]["Y"]), (p["FUL"]["X"],p["FUL"]["Y"]), (p["BUL"]["X"],p["BUL"]["Y"]), (p["BUR"]["X"],p["BUR"]["Y"]), 
-                        (p["FLL"]["X"],p["FLL"]["Y"]), (p["BLL"]["X"],p["BLL"]["Y"]), (p["BLR"]["X"],p["BLR"]["Y"]), (p["FLR"]["X"],p["FLR"]["Y"]))
+                        #print((p["FUR"]["X"],p["FUR"]["Y"]), (p["FUL"]["X"],p["FUL"]["Y"]), (p["BUL"]["X"],p["BUL"]["Y"]), (p["BUR"]["X"],p["BUR"]["Y"]), 
+                        #(p["FLL"]["X"],p["FLL"]["Y"]), (p["BLL"]["X"],p["BLL"]["Y"]), (p["BLR"]["X"],p["BLR"]["Y"]), (p["FLR"]["X"],p["FLR"]["Y"]))
                         
-                        img = cv2.line(img, (int(p["FUR"]["X"]),int(p["FUR"]["Y"])), (int(p["FUL"]["X"]),int(p["FUL"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["FUL"]["X"]),int(p["FUL"]["Y"])), (int(p["BUL"]["X"]),int(p["BUL"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["BUL"]["X"]),int(p["BUL"]["Y"])), (int(p["BUR"]["X"]),int(p["BUR"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["BUR"]["X"]),int(p["BUR"]["Y"])), (int(p["FUR"]["X"]),int(p["FUR"]["Y"])), (255, 0, 255), 1)
+                        pList = list()
+                        pList.append(np.array([p["FURGame"]["X"],p["FURGame"]["Y"],p["FURGame"]["Z"]]))
+                        pList.append(np.array([p["FULGame"]["X"],p["FULGame"]["Y"],p["FULGame"]["Z"]]))
+                        pList.append(np.array([p["BULGame"]["X"],p["BULGame"]["Y"],p["BULGame"]["Z"]]))
+                        pList.append(np.array([p["BURGame"]["X"],p["BURGame"]["Y"],p["BURGame"]["Z"]]))
+                        pList.append(np.array([p["FLLGame"]["X"],p["FLLGame"]["Y"],p["FLLGame"]["Z"]]))
+                        pList.append(np.array([p["BLLGame"]["X"],p["BLLGame"]["Y"],p["BLLGame"]["Z"]]))
+                        pList.append(np.array([p["BLRGame"]["X"],p["BLRGame"]["Y"],p["BLRGame"]["Z"]]))
+                        pList.append(np.array([p["FLRGame"]["X"],p["FLRGame"]["Y"],p["FLRGame"]["Z"]]))
 
-                        img = cv2.line(img, (int(p["FLR"]["X"]),int(p["FLR"]["Y"])), (int(p["FLL"]["X"]),int(p["FLL"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["FLL"]["X"]),int(p["FLL"]["Y"])), (int(p["BLL"]["X"]),int(p["BLL"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["BLL"]["X"]),int(p["BLL"]["Y"])), (int(p["BLR"]["X"]),int(p["BLR"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["BLR"]["X"]),int(p["BLR"]["Y"])), (int(p["FLR"]["X"]),int(p["FLR"]["Y"])), (255, 0, 255), 1)
+                        BB3D, BB2D = convertToCamCord(pList, d['Camrot'], d['Campos'], d['CamNearClip'], d['CamFOV'], d['ImageWidth'], d['ImageHeight'], d['UIwidth'], d['UIheight'])
+                        
+                        print("\n2D Bounding Boxes:")
+                        print(BB2D)
+                        print("\n3D Bounding Boxes:")
+                        print(BB3D)
+                        print("-------------------")
 
-                        img = cv2.line(img, (int(p["FUR"]["X"]),int(p["FUR"]["Y"])), (int(p["FLR"]["X"]),int(p["FLR"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["FUL"]["X"]),int(p["FUL"]["Y"])), (int(p["FLL"]["X"]),int(p["FLL"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["BUL"]["X"]),int(p["BUL"]["Y"])), (int(p["BLL"]["X"]),int(p["BLL"]["Y"])), (255, 0, 255), 1)
-                        img = cv2.line(img, (int(p["BUR"]["X"]),int(p["BUR"]["Y"])), (int(p["BLR"]["X"]),int(p["BLR"]["Y"])), (255, 0, 255), 1)
+                        if int(sys.argv[1]) == 2:
+                            for bb in BB2D:
+                                img = cv2.rectangle(img, bb[0], bb[1], (255, 255, 0), 1)
+
+                        elif int(sys.argv[1]) == 3:
+                            img = cv2.line(img, BB3D[0], BB3D[1], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[1], BB3D[2], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[2], BB3D[3], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[3], BB3D[0], (255, 0, 255), 1)
+
+                            img = cv2.line(img, BB3D[4], BB3D[5], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[5], BB3D[6], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[6], BB3D[7], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[7], BB3D[4], (255, 0, 255), 1)
+
+                            img = cv2.line(img, BB3D[0], BB3D[7], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[1], BB3D[4], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[2], BB3D[5], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[3], BB3D[6], (255, 0, 255), 1)
+
+                        elif int(sys.argv[1]) == 4:
+                            for bb in BB2D:
+                                img = cv2.rectangle(img, bb[0], bb[1], (255, 255, 0), 1)
+
+                            img = cv2.line(img, BB3D[0], BB3D[1], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[1], BB3D[2], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[2], BB3D[3], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[3], BB3D[0], (255, 0, 255), 1)
+
+                            img = cv2.line(img, BB3D[4], BB3D[5], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[5], BB3D[6], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[6], BB3D[7], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[7], BB3D[4], (255, 0, 255), 1)
+
+                            img = cv2.line(img, BB3D[0], BB3D[7], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[1], BB3D[4], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[2], BB3D[5], (255, 0, 255), 1)
+                            img = cv2.line(img, BB3D[3], BB3D[6], (255, 0, 255), 1)
+                        
+                        #img = cv2.line(img, (int(p["FUR"]["X"]),int(p["FUR"]["Y"])), (int(p["FUL"]["X"]),int(p["FUL"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["FUL"]["X"]),int(p["FUL"]["Y"])), (int(p["BUL"]["X"]),int(p["BUL"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["BUL"]["X"]),int(p["BUL"]["Y"])), (int(p["BUR"]["X"]),int(p["BUR"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["BUR"]["X"]),int(p["BUR"]["Y"])), (int(p["FUR"]["X"]),int(p["FUR"]["Y"])), (255, 0, 255), 1)
+
+                        #img = cv2.line(img, (int(p["FLR"]["X"]),int(p["FLR"]["Y"])), (int(p["FLL"]["X"]),int(p["FLL"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["FLL"]["X"]),int(p["FLL"]["Y"])), (int(p["BLL"]["X"]),int(p["BLL"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["BLL"]["X"]),int(p["BLL"]["Y"])), (int(p["BLR"]["X"]),int(p["BLR"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["BLR"]["X"]),int(p["BLR"]["Y"])), (int(p["FLR"]["X"]),int(p["FLR"]["Y"])), (255, 0, 255), 1)
+
+                        #img = cv2.line(img, (int(p["FUR"]["X"]),int(p["FUR"]["Y"])), (int(p["FLR"]["X"]),int(p["FLR"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["FUL"]["X"]),int(p["FUL"]["Y"])), (int(p["FLL"]["X"]),int(p["FLL"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["BUL"]["X"]),int(p["BUL"]["Y"])), (int(p["BLL"]["X"]),int(p["BLL"]["Y"])), (255, 0, 255), 1)
+                        #img = cv2.line(img, (int(p["BUR"]["X"]),int(p["BUR"]["Y"])), (int(p["BLR"]["X"]),int(p["BLR"]["Y"])), (255, 0, 255), 1)
+
 
                 cv2.imshow(name,img)
 
